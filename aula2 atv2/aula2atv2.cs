99023cbec6d89a88bf1fc7ee81d58c93c18a2dc7
alineazedias <aline.azedias@gmail.com>
2023-02-08 22:38:02 -0300

    atvs2

---
+++ aula2 atv2/aula2atv2.cs
@@ -0,0 +1,84 @@
+﻿// Faça um programa que possa ser usado por uma clínica para cadastrar: 30 pacientes, 
+// a data da consulta, a hora de sua realização, o nome de um paciente e o nome do médico 
+// que o atenderá. Depois de os dados serem informados, o programa deve conter uma opção que,
+// o usuário fornecendo o nome do médico, liste toda a agenda dele.
+
+// Considere que o atributo Data da consulta” seja também estruturado, formado pelos campos Dia, Mês e Ano.
+
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkBJA
+{
+    class Paciente
+    {
+        public string NomePaciente;
+        public string NomeMedico;
+        public Data DataConsulta;
+        public string HoraConsulta;
+    }
+
+    class Data
+    {
+        public string Dia;
+        public string Mes;
+        public string Ano;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paciente[] Cadastro = new Paciente[3];      //Vetor com 3 posições para teste
+
+            for (int i = 0; i < 3; i++)
+            {
+                Paciente x = new Paciente();            //objeto - intancia
+
+                Console.Write($"\nNome do Paciente nr. {i + 1}: ");
+                x.NomePaciente = Console.ReadLine();
+
+                Console.Write("Médico................: ");
+                x.NomeMedico = Console.ReadLine();
+
+                x.DataConsulta = new Data();        //muito importante - instancia
+
+                Console.Write("Data da Consulta - Dia: ");
+                x.DataConsulta.Dia = Console.ReadLine();
+
+                Console.Write("Data da Consulta - Mês: ");
+                x.DataConsulta.Mes = Console.ReadLine();
+
+                Console.Write("Data da Consulta - Ano: ");
+                x.DataConsulta.Ano = Console.ReadLine();
+
+                Console.Write("Hora da consulta......: ");
+                x.HoraConsulta = Console.ReadLine();
+
+                Cadastro[i] = x;            //cadastro feito
+            }
+            Console.Clear();
+            string NomeMedico;
+            Console.Write("Digite o nome de um medico: ");
+            NomeMedico = Console.ReadLine();
+
+            foreach (Paciente P in Cadastro)
+            {
+                if (P.NomeMedico == NomeMedico)
+                {
+                    Console.WriteLine($"\nPaciente: {P.NomePaciente}");
+                    Console.WriteLine($"{P.DataConsulta.Dia}/" +
+                                      $"{P.DataConsulta.Mes}/" +
+                                      $"{P.DataConsulta.Ano} - " +
+                                      $"{P.HoraConsulta} Horas");
+                }
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ aula2 video2/aula2video2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkBJA
+{
+    class Veiculo                       //classe de tipo
+    {
+        public string Nome;
+        public string Marca;
+        public string AnoFab;
+        public string Placa;
+        public double PreçoFIP;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vetor para "armazenar" veiculos...
+            Veiculo[] CadVeiculo = new Veiculo[3];      //3 veiculos, para exemplo...
+
+            for (int i = 0; i < 3; i++)
+            {
+                Veiculo x = new Veiculo();              //instancia - atenção...
+
+                Console.Write($"\nNome do veículo{i + i}......: ");
+                x.Nome = Console.ReadLine();
+
+                Console.Write("Marca..............: ");
+                x.Marca = Console.ReadLine();
+
+                Console.Write("Ano de Fabricação.......: ");
+                x.AnoFab = Console.ReadLine();
+
+                Console.Write("Placa..............: ");
+                x.Placa = Console.ReadLine();
+
+                Console.Write("Preço tabela FIP.........: ");
+                x.PreçoFIP = double.Parse(Console.ReadLine());
+
+                //Vamos colocar o objeto x no vetor, posição i...
+                CadVeiculo[i] = x;          //cadastrou!
+            }
+            Console.Clear();
+            //msotrando todos os veiculos cadastrados...
+
+            foreach (Veiculo V in CadVeiculo)
+            {
+                Console.WriteLine($"\n{V.Nome} - Ano: {V.AnoFab}");
+                Console.WriteLine($"Marca: {V.Marca}");
+                Console.WriteLine($"Placa: {V.Placa}");
+                Console.WriteLine($"Preço - Tabela FIP - R${V.PreçoFIP:F2}");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
